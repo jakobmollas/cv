510f2d5a7b61d763a064dee1d262bbd561bd3c0b
Jakob Möllås <jakob.mollas@gmail.com>
2021-07-28 16:59:33 +0200

    Convert index.html into handlebar template + json content

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ Custom-made CV for Jakob Möllås: https://jakobmollas.github.io/cv/
 
 LinkedIn: https://www.linkedin.com/in/jakobmollas/
 
-Contains no dependencies except for Google fonts.
-
+Using Handlebars template to generate a static site from JSON contents.
---
+++ src/compile-template.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var path = require('path');
+var Handlebars = require("handlebars");
+
+var templatePath = process.argv[2];
+var contentPath = process.argv[3];
+var outputPath = process.argv[4];
+
+console.log("Template:", templatePath);
+console.log("Content data:", contentPath);
+console.log("Output file:", outputPath);
+
+var content = JSON.parse(fs.readFileSync(contentPath, "utf-8"));
+compileTemplate(content);
+
+function compileTemplate(content) {
+  var template = fs.readFileSync(templatePath, "utf-8");
+  var renderTemplate = Handlebars.compile(template);
+  var generatedText = renderTemplate(content);
+  
+  fs.writeFile(outputPath, generatedText, err => {
+    if (err) console.log(err);
+    
+    console.log("");
+    console.log("Succesfully generated", outputPath);
+  });
+}
